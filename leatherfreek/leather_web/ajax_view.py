9cc7f098416372038679f9d2a5999ceadafd8099
Karuna Kandel <kandelkaruna2060@gmail.com>
2024-07-02 12:26:32 -0400

    20th commit- fixed shopping cart behaviour

--- leatherfreek/leather_web/ajax_view.py
+++ leatherfreek/leather_web/ajax_view.py
@@ -3,19 +3,22 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 from .models import Display_Product, shopping_cart , User_Record
 
-@require_POST
 def add_to_cart_ajax(request, product_id):
     product = get_object_or_404(Display_Product, product_id=product_id)
     
-    # Check if the user is authenticated
     if request.user.is_authenticated:
-        # User is logged in, associate cart with user
-        cart_item, created = shopping_cart.objects.get_or_create(user=request.user, product=product)
-
-        if not created:
-            # If the item already exists in the cart, increment quantity
+        # User is logged in
+        try:
+            # Try to get the existing cart item
+            cart_item = shopping_cart.objects.get(user=request.user, product=product)
+            # If it exists, increase the quantity by 1
             cart_item.quantity += 1
-            cart_item.save()
+        except shopping_cart.DoesNotExist:
+            # If it doesn't exist, create a new cart item with quantity 1
+            cart_item = shopping_cart(user=request.user, product=product, quantity=1)
+        
+        # Save the cart item
+        cart_item.save()
     else:
         # User is not authenticated, use session-based cart
         if 'cart' not in request.session:
@@ -24,10 +27,10 @@ def add_to_cart_ajax(request, product_id):
         cart = request.session['cart']
 
         # Check if the product already exists in the session cart
-        if product_id in cart:
-            cart[product_id]['quantity'] += 1
+        if str(product_id) in cart:
+            cart[str(product_id)]['quantity'] += 1
         else:
-            cart[product_id] = {
+            cart[str(product_id)] = {
                 'product_id': product_id,
                 'quantity': 1,
                 # You can also store other product details in the session if needed
@@ -38,8 +41,6 @@ def add_to_cart_ajax(request, product_id):
         print(request.session['cart'])
 
     return JsonResponse({'success': True, 'product': product.product_name})
-
-
 
 def increase_quantity_ajax(request, product_id):
     product = get_object_or_404(Display_Product, product_id=product_id)
